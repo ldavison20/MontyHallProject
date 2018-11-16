ede64416082102a7c19f61ea4f06597eb94aee62
ldavison20 <43002623+ldavison20@users.noreply.github.com>
2018-11-16 10:15:20 -0500

    Add files via upload

---
+++ MontyHall.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+public class MontyHall 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		Random rand = new Random();
+		System.out.println("Choose a door you want to pick: ");
+		String door = keyboard.nextLine();
+		
+	}
+
+}
